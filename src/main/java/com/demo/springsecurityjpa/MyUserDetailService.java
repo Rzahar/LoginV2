1ffbc1b5930a34419a07638f1c8a76d5c828ad3b
Rachid ZAHAR <rachid.zahar@sii.fr>
2022-03-10 10:51:51 +0100

    login seconde version

---
+++ src/main/java/com/demo/springsecurityjpa/MyUserDetailService.java
@@ -0,0 +1,26 @@
+package com.demo.springsecurityjpa;
+
+import com.demo.springsecurityjpa.model.MyUserDetails;
+import com.demo.springsecurityjpa.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+       Optional<User>user= userRepository.findByUserName(userName);
+       user.orElseThrow(()-> new UsernameNotFoundException("Not found : " + userName ));
+       return user.map(MyUserDetails::new).get();
+    }
+}
---
+++ src/main/java/com/demo/springsecurityjpa/UserRepository.java
@@ -0,0 +1,10 @@
+package com.demo.springsecurityjpa;
+
+import com.demo.springsecurityjpa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User>findByUserName(String userName);
+}
